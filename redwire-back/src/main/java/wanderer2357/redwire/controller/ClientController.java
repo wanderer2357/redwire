f1dd2658852b40a9296e3714b922f318d3baa9ef
wanderer <younes.mabrouk@laposte.net>
2025-11-26 21:08:12 +0000

    fixed unused imports and upgraded to latest spring boot patch

--- redwire-back/src/main/java/wanderer2357/redwire/controller/ClientController.java
+++ redwire-back/src/main/java/wanderer2357/redwire/controller/ClientController.java
@@ -16,9 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
--- redwire-back/src/main/java/wanderer2357/redwire/controller/SupplierController.java
+++ redwire-back/src/main/java/wanderer2357/redwire/controller/SupplierController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
